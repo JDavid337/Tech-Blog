4eacb5d06375124d6e73017678080cec70777170
Captain Ahab <captainahab337@gmail.com>
2021-07-26 10:39:41 -0600

    updated seeds, server.js, utils, and views

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const { User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+    const userData = await User.findAll({
+/*        include: [
+            {
+                model: User,
+                attributes: ['name'],
+        attributes: { exclude: ['password'] },
+        order: [['name', 'ASC']],
+    }]*/
+    });
+console.log(userData)
+    const users = userData.map((project) => project.get({ plain: true }));
+
+    res.render('homepage', {
+        users,
+        logged_in: req.session.logged_in,
+    });
+    } catch (err) {
+    res.status(500).json(err);
+    }
+});
+router.get('/posts', withAuth, async (req, res) => {
+    try {
+ //   const userData = await User.findAll({})
+res.render("posts")
+    } catch(err){
+        res.json(err)
+    }
+    })
+router.get('/signup', (req, res) => {
+
+    res.render('signup');
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+    }
+
+    res.render('login');
+});
+
+module.exports = router;
---
+++ models/Posts.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Project extends Model {}
+
+Project.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    user_email: {
+      type: DataTypes.STRING,
+      references: {
+        model: 'user',
+        key: 'email',
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'project',
+  }
+);
+
+module.exports = Project;
