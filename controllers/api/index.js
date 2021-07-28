1faf63b4495e60ef03878ef489d0328b3d9b8fc8
Captain Ahab <captainahab337@gmail.com>
2021-07-28 11:25:43 -0600

    updated index.js, added post and test routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
+const testRoute = require('./testRoute');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('comments', commentRoutes);
+router.use('/test', testRoute);
 
 module.exports = router;
